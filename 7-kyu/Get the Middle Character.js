addf7fa5f2b03d3958fc2fb2d3c91d2b572a7fce
Michael Futral <MichaelFutral01@gmail.com>
2022-05-16 23:34:41 -0400

    codewars 20220516

---
+++ 7-kyu/Get the Middle Character.js
@@ -0,0 +1,15 @@
+function getMiddle(s) {
+
+    return s.substr(Math.ceil(s.length / 2 - 1),
+        s.length % 2 === 0 ? 2 : 1);
+}
+
+
+//   function getMiddle(s){
+
+//     let middle = Math.floor(s.length/2);
+
+//     return s.length % 2 === 0
+//         ? s.slice(middle-1, middle+1)
+//         : s.slice(middle, middle+1);
+//     }
---
+++ 7-kyu/Number of People in the Bus.js
@@ -0,0 +1,15 @@
+var number = function (busStops) {
+    // Good Luck!
+
+    let totalPpl = 0
+
+    for (let i = 0; i < busStops.length; i++) {
+        let currStop = busStops[i]
+        let getOn = currStop[0]
+        let getOff = currStop[1]
+
+        totalPpl += getOn;
+        totalPpl -= getOff;
+    }
+    return totalPpl;
+}
