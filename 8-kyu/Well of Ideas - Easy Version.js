6d405f7a72bf464555c799a9bccb33f06a50057f
Michael Futral <MichaelFutral01@gmail.com>
2022-08-21 13:54:36 -0400

    20200821

--- 8-kyu/Well of Ideas - Easy Version.js
+++ 8-kyu/Well of Ideas - Easy Version.js
@@ -5,3 +5,12 @@ function well(x) {
 
     return count < 1 ? 'Fail!' : count <= 2 ? 'Publish!' : 'I smell a series!'
 }
+
+//or
+
+function well(x) {
+
+    let count = x.reduce((pre, curr) => curr == 'good' ? pre += 1 : pre, 0)
+
+    return count < 1 ? 'Fail!' : count <= 2 ? 'Publish!' : 'I smell a series!'
+}
